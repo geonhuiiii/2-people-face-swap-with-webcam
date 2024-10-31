dd9660a616c87b1dd7c843fab3b667f8654b87c7
geonhuiii <geonhui@aidraw.co.kr>
2024-10-31 11:50:04 +0900

    수정

---
+++ pyfacefromfile2.py
@@ -0,0 +1,27 @@
+import insightface, cv2
+from insightface.app import FaceAnalysis
+from insightface.app.common import Face
+
+cap = cv2.VideoCapture(0)
+ret, frame = cap.read() 
+app = FaceAnalysis(name="buffalo_l",allowed_modules=['detection','recognition'], providers=['CPUExecutionProvider', 'CUDAExecutionProvider'])
+app.prepare(ctx_id=0, det_thresh=0.5, det_size=(640, 640))
+swapper = insightface.model_zoo.get_model('./inswapper_128_fp16.onnx',providers=['CPUExecutionProvider', 'CUDAExecutionProvider'])
+source = cv2.imread("./source.jpg")
+
+if ret:
+    try:
+        myfaces = app.get(frame)
+        sourcefaces = app.get(source)
+        result = swapper.get(source, sourcefaces[0], myfaces[0])
+        cv2.imwrite("./changed.jpg", result) 
+        cv2.imshow('cam', result)
+        print("얼굴바꾸기 성공! changed.jpg 파일에 저장")
+        cv2.waitKey()
+    except Exception as E:
+        cv2.imshow('cam', frame)
+        print("얼굴바꾸기 실패...")
+        print(E)
+        cv2.waitKey()
+cap.release()
+cv2.destroyAllWindows()
